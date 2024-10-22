43b16a90a193055a1cf411301803eec7ab8fd279
Piotr Paczek <ppaczek1@o2.pl>
2024-10-22 13:13:40 +0200

    test_commit

--- src/main/java/org/main_example/Main.java
+++ src/main/java/org/main_example/Main.java
@@ -2,6 +2,6 @@ package org.main_example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Welcome to Hotel managing service app.");
     }
 }
