b1984b22dcb6900697d6061fe0239443b28ad339
Piotr Paczek <ppaczek1@o2.pl>
2024-11-15 15:03:38 +0100

    checkincommand simplified

--- main/src/main/java/org/example/lab/agh/command_package/CheckinCommand.java
+++ main/src/main/java/org/example/lab/agh/command_package/CheckinCommand.java
@@ -23,40 +23,33 @@ public class CheckinCommand implements Command{
             int userChoice = tempScanner.nextInt();
             tempScanner.nextLine();  // Czyści znak nowej linii Enter po nextInt()
 
-            if(tempHotel.getRoomsMap().contains(userChoice)){
-                Room chosenRoom = tempHotel.getRoomsMap().get(userChoice);
-
-                if(chosenRoom.getRoomRegisteredGuests().isEmpty()){
-                    System.out.printf("How many guests? {%d} : ",chosenRoom.getMaxGuestNumber());
-                    int howManyGuests = tempScanner.nextInt();
-                    tempScanner.nextLine();  // Czyści znak nowej linii Enter po nextInt()
-                    if(howManyGuests <= chosenRoom.getMaxGuestNumber()){
-                        for(int i = 1; i <= howManyGuests; i++){
-                            System.out.printf("Please register %d. guest >> %n", i);
-                            chosenRoom.registerGuest(tempScanner);
-                        }
-                        System.out.printf("Checkin date is set to %s %n", LocalDate.now());
-                        chosenRoom.setCheckinDate(LocalDate.now());
-                        System.out.print("Please set te checkout date (YYYY-MM-DD): ");
-                        String checkoutDateString = tempScanner.nextLine().trim();
-                        LocalDate checkoutDate = LocalDate.parse(checkoutDateString);
-                        chosenRoom.setCheckoutDate(checkoutDate);
-                        break;
-                    }
-                    else{
-                        System.out.print("Sorry, this room is not big enough for that many people, please choose different one: ");
-                    }
-                }
-                else{
-                    System.out.print("Chosen room is currently occupied, please choose different one: ");
-                }
-            }
-            else{
+            if(!tempHotel.getRoomsMap().contains(userChoice)){
                 System.out.print("Chosen room doesnt exist, please choose different room: ");
+                continue;
             }
-
+            Room chosenRoom = tempHotel.getRoomsMap().get(userChoice);
+            if(!chosenRoom.getRoomRegisteredGuests().isEmpty()){
+                System.out.print("Chosen room is currently occupied, please choose different one: ");
+                continue;
+            }
+            System.out.printf("How many guests? {%d} : ",chosenRoom.getMaxGuestNumber());
+            int howManyGuests = tempScanner.nextInt();
+            tempScanner.nextLine();  // Czyści znak nowej linii Enter po nextInt()
+            if(howManyGuests > chosenRoom.getMaxGuestNumber()){
+                System.out.print("Sorry, this room is not big enough for that many people, please choose different one: ");
+                continue;
+            }
+            for(int i = 1; i <= howManyGuests; i++){
+                System.out.printf("Please register %d. guest >> %n", i);
+                chosenRoom.registerGuest(tempScanner);
+            }
+            System.out.printf("Checkin date is set to %s %n", LocalDate.now());
+            chosenRoom.setCheckinDate(LocalDate.now());
+            System.out.print("Please set te checkout date (YYYY-MM-DD): ");
+            String checkoutDateString = tempScanner.nextLine().trim();
+            LocalDate checkoutDate = LocalDate.parse(checkoutDateString);
+            chosenRoom.setCheckoutDate(checkoutDate);
+            break;
         }
-
-
     }
 }
